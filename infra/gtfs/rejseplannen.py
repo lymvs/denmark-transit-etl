2a33126675a18b68e7142ac060516de919192094
lymvs <lysandros.mavropoulos@gmail.com>
2026-04-21 20:07:14 +0000

    download zipfiles with gtfs files

--- infra/gtfs/rejseplannen.py
+++ infra/gtfs/rejseplannen.py
@@ -1,8 +1,8 @@
 import logging
+from io import BytesIO
+from zipfile import ZipFile
 
 import requests
-from zipfile import ZipFile
-from io import BytesIO
 
 logger = logging.getLogger(__name__)
 
@@ -10,11 +10,13 @@ URL = "https://www.rejseplanen.info/labs/GTFS.zip"
 
 
 def download_files() -> None:
-    response = requests.get(URL)
+    """Download zipfile and extract files to directory."""
+    response = requests.get(URL, timeout=10)
 
     if response.status_code == 200:
         with ZipFile(BytesIO(response.content)) as zip_file:
             zip_file.extractall(path="temp/")
         logger.info("Files extracted successfully.")
     else:
-        raise Exception(f"Failed to download files: {response.text}")
+        msg = f"Failed to downlaod files: {response.text}"
+        raise Exception(msg)
