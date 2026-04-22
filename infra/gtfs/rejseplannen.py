d1a4810e1dbbc90105c336938de9807cef0e0ac5
lymvs <lysandros.mavropoulos@gmail.com>
2026-04-22 20:45:51 +0000

    apply fixes for ruff alignment

--- infra/gtfs/rejseplannen.py
+++ infra/gtfs/rejseplannen.py
@@ -1,3 +1,4 @@
+"""Download and store zipfiles for GTFS data."""
 import logging
 from io import BytesIO
 from zipfile import ZipFile
@@ -11,12 +12,17 @@ URL = "https://www.rejseplanen.info/labs/GTFS.zip"
 
 def download_files() -> None:
     """Download zipfile and extract files to directory."""
-    response = requests.get(URL, timeout=10)
-
-    if response.status_code == 200:
+    try:
+        response = requests.get(URL, timeout=10)
+        response.raise_for_status()
         with ZipFile(BytesIO(response.content)) as zip_file:
             zip_file.extractall(path="temp/")
         logger.info("Files extracted successfully.")
-    else:
-        msg = f"Failed to downlaod files: {response.text}"
-        raise Exception(msg)
+    except requests.exceptions.HTTPError as e:
+        logger.info("HTTP error occured: %e", e)
+    except requests.exceptions.ConnectionError as e:
+        logger.info("Connection error occured: %e", e)
+    except requests.exceptions.Timeout as e:
+        logger.info("Timeout error occured: %s", e)
+    except requests.exceptions.RequestException as e:
+        logger.info("An unexcpected error occured: %s", e)
